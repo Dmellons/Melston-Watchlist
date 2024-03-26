7271eb8e27ac72f3d25a9aeddf2b43e51cb3eaca
David Mellons <dmellons@gmail.com>
2024-03-26 10:22:39 -0700

    Working add to watchlist button on /test/

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -2,9 +2,10 @@ import { Client, Account, Databases } from 'appwrite';
 
 export const client = new Client();
 
+
 client
     .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT_URL as string)
-    .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID as string);
+    .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID as string)
 
 export const account = new Account(client);
 export const database = new Databases(client)
--- types/appwrite.d.ts
+++ types/appwrite.d.ts
@@ -1,24 +1,24 @@
 import { PlatformPath } from "path"
 
 export type AppwriteUser = {
-    
-        "$id": string,
-        "$createdAt": string,
-        "$updatedAt": string,
-        "name": string,
-        "password": string,
-        "hash": string,
-        "hashOptions": object
-        "registration": string,
-        "status": bool,
-        "labels": string,
-        "passwordUpdate": string,
-        "email": string,
-        "phone": string,
-        "emailVerification": boolean,
-        "phoneVerification": bool,
-        "prefs": UserPrefs,
-        "accessedAt": string
+
+    "$id": string,
+    "$createdAt": string,
+    "$updatedAt": string,
+    "name": string,
+    "password": string,
+    "hash": string,
+    "hashOptions": object
+    "registration": string,
+    "status": bool,
+    "labels": string,
+    "passwordUpdate": string,
+    "email": string,
+    "phone": string,
+    "emailVerification": boolean,
+    "phoneVerification": bool,
+    "prefs": UserPrefs,
+    "accessedAt": string
 } | Models.User<Models.Preferences>
 
 export type UserPrefs = {
@@ -30,26 +30,30 @@ export type UserRoles = [
     'User'
 ]
 
-export enum ContentType = {
-    boardgame,
-    videogame,
-    tv,
-    movie
+export enum ContentType {
+    boardgame = 'boardgame',
+    book = 'book',
+    music = 'music',
+    videogame = 'videogame',
+    tv = 'tv',
+    movie = 'movie'
 }
 
-export enum platform {
-    Netflix,
-    Nintendo,
-    Xbox,
-    Playstaation,
-    Hulu,
-    Max,
-    CrunchyRoll,
+export enum Platform {
+    Netflix = 'Netflix',
+    Nintendo = 'Nintendo',
+    AmazonPrime = 'AmazonPrime',
+    AppleTv = 'AppleTv',
+    Xbox = 'Xbox',
+    PlayStation = 'Playstation',
+    Hulu = 'Hulu',
+    Max = 'Max',
+    CrunchyRoll = 'CrunchyRoll'
 }
 
 export type WatchlistDocumentCreate = {
     title: string,
-    content_type: ContentType[],
-    patform: Platform[],
+    contentType: ContentType[],
+    platform: Platform[],
 
 }
