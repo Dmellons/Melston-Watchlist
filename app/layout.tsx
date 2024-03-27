4242a64f9459c85cf99517a71c748d209cc38df4
David Mellons <dmellons@gmail.com>
2024-03-27 08:28:16 -0700

    Added favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,6 +21,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head >
+      <link rel="icon" href="/logo.svg" sizes="any" />
+        </head>
       <body className={inter.className}>
         <UserProvider>
           <ThemeProvider
