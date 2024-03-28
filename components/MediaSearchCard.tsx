0fc9d4d835285bdb2f5225968fcb0ceab9bd1df9
David Mellons <dmellons@gmail.com>
2024-03-28 11:59:23 -0700

    Updated debug

--- components/MediaSearchCard.tsx
+++ components/MediaSearchCard.tsx
@@ -45,7 +45,7 @@ const MediaSearchCard = ({
                 />
             </CardHeader>
             {
-            process.env.NEXT_PUBLIC_USER_DEBUG === "false" &&
+            process.env.NEXT_PUBLIC_USER_DEBUG === "true" &&
             <CardContent>
                 <p className="text-sm text-muted-foreground">
                     {media.type}
