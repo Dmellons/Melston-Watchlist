59a7c566b4d2c0819899e639407a8c0621473507
David Mellons <dmellons@gmail.com>
2024-03-28 17:06:13 -0700

    fixed narrow display on phones when searching.

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import Image from "next/image";
 export default function Home() {
   const { user } = useUser()
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-2 sm:p-24">
       {user &&
         <SearchMovie />
       }
