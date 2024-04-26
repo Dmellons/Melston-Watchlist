3e492432a4852bd02e27c14f2c27d4a66bb5d303
David Mellons <dmellons@gmail.com>
2024-04-26 15:20:28 -0700

    updated tv detail page to match movie page

--- app/tv/[tmdbId]/page.tsx
+++ app/tv/[tmdbId]/page.tsx
@@ -1,211 +1,152 @@
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion";
+import { Button } from "@/components/ui/button";
+import { Label } from "@/components/ui/label";
+import Image from 'next/image';
+import Link from 'next/link';
 import CastAndCrew from '@/components/CastAndCrew';
 import ProvidersBlock from '@/components/ProvidersBlock';
 import AddWatchlistButton from '@/components/buttons/AddWatchlistButton';
 import BackButton from '@/components/buttons/BackButton';
-import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from '@/components/ui/accordion';
-import { Button } from '@/components/ui/button';
-import { Label } from '@/components/ui/label';
-import { TMDBApiMovieDetail, TMDBApiTvDetail } from '@/lib/tmdb';
-import { tmdbFetchOptions } from '@/lib/tmdb';
-import { TMDBMultiSearchResult } from '@/types/tmdbApi';
-import Image from 'next/image';
-import Link from 'next/link';
+import { TMDBApiMovieDetail, TMDBApiTvDetail, tmdbFetchOptions } from "@/lib/tmdb";
 
 interface DetailPageProps {
-
   params: {
     tmdbId: string;
   }
 }
 
-export const DetailPage = async ({ params }: DetailPageProps) => {
+const DetailPage = async ({ params }: DetailPageProps) => {
   const { tmdbId } = params;
-  const tmdbType = 'tv'
+  const tmdbType = 'tv';
 
-  console.log({ tmdbType, tmdbId })
-  const url = `https://api.themoviedb.org/3/tv/${tmdbId}?append_to_response=credits&language=en-US`;
+  const url = `https://api.themoviedb.org/3/${tmdbType}/${tmdbId}?append_to_response=credits&language=en-US`;
   const response = await fetch(url, tmdbFetchOptions);
   const data: TMDBApiTvDetail = await response.json();
-  console.log({ data })
 
-  const addButtonData: Partial<TMDBMultiSearchResult> = {
+  const addButtonData = {
     id: data.id,
     title: data.name,
-    poster_path: data.poster_path
-  }
+    poster_path: data.poster_path,
+  };
 
   return (
-    <div className="container mx-auto px-4 py-8 md:px-8">
-      <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
-        <div className="relative">
-          <BackButton />
+    <div className="container mx-auto p-6 md:p-8 lg:p-12">
+      <BackButton className="mb-4" />
+
+      <div className="grid grid-cols-1 gap-6 md:grid-cols-2">
+        {/* Poster Image */}
+        <div className="relative rounded-lg overflow-hidden shadow-lg">
           <Image
             src={`https://image.tmdb.org/t/p/w500${data.poster_path}`}
             alt={data.name}
-            className="w-full rounded-lg shadow-md"
+            className="w-full"
             width={500}
-            height={500}
+            height={750}
+            priority
           />
-          {/* <div className="absolute bottom-0 left-0 p-2">
-            <p className="text-white text-xs font-bold bg-black bg-opacity-70 rounded-b-lg px-1">
-              Release Date: {data.release_date}
-            </p>
-          </div> */}
+          <div className="absolute bottom-0 left-0 bg-black bg-opacity-70 text-white text-sm p-2">
+            First Air Date: {data.first_air_date}
+          </div>
         </div>
-        <div className="space-y-4 sm:mt-10">
+
+        {/* Movie Details */}
+        <div className="space-y-6">
           <h1 className="text-3xl font-bold">{data.name}</h1>
-          <p className="text-gray-700">{data.tagline}</p>
-          <p>{data.overview}</p>
-          <div className="flex items-start gap-2">
+          <p className="text-gray-600">{data.tagline}</p>
+          <p className="text-gray-700">{data.overview}</p>
 
-            <Label className='flex-1 text-right justify-end'
-              htmlFor='streaming' >Streaming on</Label>
-            <ProvidersBlock
-              tmdbId={data.id}
-              tmdbType={tmdbType}
-              maxWidth='96'
-              iconSize={48}
-              userProviders
-
-            />
+          <div className="flex items-center gap-2">
+            <Label htmlFor="streaming">Streaming on</Label>
+            <ProvidersBlock tmdbId={data.id} tmdbType={tmdbType} iconSize={48} />
           </div>
+
           {data.vote_average > 0 && (
-            <p>Score: {data.vote_average.toFixed(1)}/10 <span className="text-foreground/20 text-xs">({data.vote_count} votes)</span></p>
+            <p>
+              Score: {data.vote_average.toFixed(1)}/10{" "}
+              <span className="text-foreground/50 text-sm">({data.vote_count} votes)</span>
+            </p>
           )}
-          {
-            data.genres?.length > 0 ? (
-              <p>Genres: {data.genres.map((genre) => genre.name).join(', ')}</p>
-            ) : (
-              <p>No genres available</p>
-            )
-          }
-          <p>Status: {data.status}</p>
-          {data.first_air_date && data.last_air_date && (<>
-            <p>First Air Date: {new Date(data.first_air_date).toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' })} </p>
-            <p>Last Air Date: {new Date(data.last_air_date).toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' })} </p>
-            <p>Seasons: {data.number_of_seasons}</p>
-            <p>Episodes: {data.number_of_episodes}</p>
-            <p>Last Episode to Air: {data.last_episode_to_air?.name} <span className="text-xs text-foreground/25">{new Date(data.last_episode_to_air?.air_date).toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' })}</span></p>
-          </>
+
+          {data.genres?.length > 0 ? (
+            <p>Genres: {data.genres.map((genre) => genre.name).join(", ")}</p>
+          ) : (
+            <p>No genres available</p>
           )}
+
           <p>Language: {data.original_language}</p>
-
-
-          <div className="p-4 text-center sm:text-left">
-
-            <h3 className="text-lg font-bold">External Links</h3>
-            <div className="flex divide-x justify-center sm:justify-normal">
-
-              <Button
-                variant={"link"}
-                asChild
-              >
-
-                <Link
-                  href={`https://www.imdb.com/title/${data.imdb_id}`}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  IMDB
-                </Link>
-              </Button>
-              <Button
-                variant={"link"}
-                asChild
-              >
-
-                <Link
-                  href={data.homepage}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Homepage
-                </Link>
-              </Button>
-            </div>
-          </div>
-
-
-          <Accordion type='single' collapsible className=''>
-            {
-              data.production_countries?.length > 0 ? (
-                
-                
-                <AccordionItem value={'Production Countries'} className='w-full sm:w-fit'>
-                  <AccordionTrigger className='text-center justify-center sm:justify-start'>
-                    <h3 className="text-lg font-bold text-center sm:text-left mr-2">Production Countries:</h3>
-                  </AccordionTrigger>
-                  <AccordionContent>
-                    <div className='sm:mr-4'>
-                      {data.production_countries?.map(country => (
-                        <div className="text-sm text-center sm:ml-4 sm:text-left" key={country.iso_3166_1}>{country.name}</div>
-                      ))}
-                    </div>
-                  </AccordionContent>
-                </AccordionItem>
-              ) : (
-                <div>No production countries available</div>
-              )
-            }
-            {
-              data.production_companies?.length > 0 ? (
-                
-                <AccordionItem value={'Production Companies'} className='w-full sm:w-fit'>
-                  <AccordionTrigger className='text-center justify-center sm:justify-start'>
-                    <h3 className="text-lg font-bold text-center sm:text-left mr-2">Production Companies:</h3>
-                  </AccordionTrigger>
-                  <AccordionContent>
-                    <div className='sm:mr-4'>
-                      {data.production_companies.map(company => (
-                        <div className="text-sm text-center sm:ml-4 sm:text-left" key={company.id}>{company.name}</div>
-                      ))}
-                    </div>
-                  </AccordionContent>
-                </AccordionItem>
-              ) : (
-                <div>No production companies available</div>
-              )
-            }
-          </Accordion>
-          {/* <Accordion type='single' collapsible className=''>
-            <AccordionItem value={'Production Countries'} className='w-full sm:w-fit'>
-              <AccordionTrigger className='text-center justify-center sm:justify-start'>
-                <h3 className="text-lg font-bold text-center sm:text-left mr-2">Production Companies:</h3>
-              </AccordionTrigger>
-              <AccordionContent>
-                <div className='sm:mr-4'>
-                {data.production_companies.map(company => (
-                  <div className="text-sm text-center sm:ml-4 sm:text-left" key={company.id}>{company.name}</div>
-                  ))}
-                </div>
-              </AccordionContent>
-            </AccordionItem>
-          </Accordion> */}
-
-
+          <p>Status: {data.status}</p>
+          <p>Runtime: {data.episode_run_time} minutes</p>
+          <p>Seasons: {data.number_of_seasons}</p>
+          <p>Episodes: {data.number_of_episodes}</p>
+          {data.next_episode_to_air && <p>Next Air Date: {data.next_episode_to_air?.air_date}</p>}
 
           
-
-          <div className="hidden">
-
-            <AddWatchlistButton
-              media={addButtonData}
-              tmdbId={tmdbId}
-              width=""
-              query
-            />
-          </div>
-              <CastAndCrew 
-                cast={data.credits.cast} 
-                type='cast'
-                />
         </div>
       </div>
+
+      {/* Accordion Section */}
+      <Accordion type="single" collapsible>
+        {data.production_countries?.length > 0 && (
+          <AccordionItem value="Production Countries">
+            <AccordionTrigger>
+              <h3 className="text-lg font-bold">Production Countries</h3>
+            </AccordionTrigger>
+            <AccordionContent>
+              <div className="pl-4">
+                {data.production_countries.map((country) => (
+                  <p key={country.iso_3166_1}>{country.name}</p>
+                ))}
+              </div>
+            </AccordionContent>
+          </AccordionItem>
+        )}
+
+        {data.production_companies?.length > 0 && (
+          <AccordionItem value="Production Companies">
+            <AccordionTrigger>
+              <h3 className="text-lg font-bold">Production Companies</h3>
+            </AccordionTrigger>
+            <AccordionContent>
+              <div className="pl-4">
+                {data.production_companies.map((company) => (
+                  <p key={company.id}>{company.name}</p>
+                ))}
+              </div>
+            </AccordionContent>
+          </AccordionItem>
+        )}
+      </Accordion>
+
+      {/* Cast and Crew Section */}
+      <CastAndCrew cast={data.credits.cast} type="cast" />
+
+      {/* External Links */}
+      <div className="pt-6">
+        <h3 className="text-lg font-bold">External Links</h3>
+        <div className="flex divide-x divide-gray-400">
+          <Button variant="link" asChild>
+            <Link
+              href={`https://www.imdb.com/title/${data.id}`}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              IMDB
+            </Link>
+          </Button>
+          <Button variant="link" asChild>
+            <Link
+              href={data.homepage}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Homepage
+            </Link>
+          </Button>
+        </div>
+      </div>
+
     </div>
   );
 };
 
-
 export default DetailPage;
-
-
