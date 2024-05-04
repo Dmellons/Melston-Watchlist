44492f190a375444897e353249cf7eea982ce1fe
David Mellons <dmellons@gmail.com>
2024-05-04 04:37:20 -0700

    Providers block hangs on loading before plex db call

--- types/env.d.ts
+++ types/env.d.ts
@@ -7,13 +7,12 @@ namespace NodeJS {
         NEXT_PUBLIC_APPWRITE_PROJECT_ID: string;
         APPWRITE_API_KEY: string;
         NEXT_PUBLIC_APPWRITE_WATCHLIST_COLLECTION_ID: string;
-
+        NEXT_PUBLIC_APPWRITE_PLEX_COLLECTION_ID: string;
+        
         NEXT_PUBLIC_WATCHMODE_API_KEY: string;
 
         NEXT_PUBLIC_TMDB_API_KEY: string;
         NEXT_PUBLIC_TMDB_API_READ_ACCESS_TOKEN: string;
-
-        NEXT_PUBLIC_PLEX_TOKEN: string;
 
         COOKIE_NAME: string;
 
