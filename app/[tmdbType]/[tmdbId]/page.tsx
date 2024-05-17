f069075d3cd2cf914bb8d56c3c584c73537863ef
David Mellons <dmellons@gmail.com>
2024-05-16 20:35:38 -0700

    Finished single details template

--- app/[tmdbType]/[tmdbId]/page.tsx
+++ app/[tmdbType]/[tmdbId]/page.tsx
@@ -19,81 +19,87 @@ interface DetailPageProps {
     }
 }
 
+function isMovieDetail(data: TMDBApiMovieDetail | TMDBApiTvDetail): data is TMDBApiMovieDetail {
+    return (data as TMDBApiMovieDetail).title !== undefined;
+}
+
+function isTvDetail(data: TMDBApiMovieDetail | TMDBApiTvDetail): data is TMDBApiTvDetail {
+    return (data as TMDBApiTvDetail).first_air_date !== undefined;
+}
+
+
 const DetailPage = async ({ params }: DetailPageProps) => {
     const { tmdbId, tmdbType } = params;
-    console.log({ tmdbId, tmdbType })
-    //   const tmdbType = 'movie';
+
+
     const url = `https://api.themoviedb.org/3/${tmdbType}/${tmdbId}?append_to_response=credits&language=en-US`;
     const response = await fetch(url, tmdbFetchOptions);
-    
+
     if (!response.ok) {
         throw new Error('Failed to fetch data');
     }
-    // @ts-ignore
-    let data: TMDBApiMovieDetail | TMDBApiTvDetail = {}
-    
-    if (tmdbType === 'movie') {
-        data = await response.json() as TMDBApiMovieDetail;
+
+    let data: TMDBApiMovieDetail | TMDBApiTvDetail = await response.json();
+
+    if (!data) {
+        throw new Error('Failed to fetch data');
     }
 
-    if (tmdbType === 'tv') {
-        data = await response.json() as TMDBApiTvDetail;
-    }
 
-    console.log({data})
-    
 
     const addButtonData: WatchlistDocumentCreate = {
         tmdb_id: data.id,
         tmdb_type: tmdbType,
-        title: data?.title ?? data?.name,
+        title: isMovieDetail(data) ? data.title : data.name,
         poster_url: `https://image.tmdb.org/t/p/w500${data.poster_path}`,
         backdrop_url: `https://image.tmdb.org/t/p/w500${data.backdrop_path}`,
         content_type: 'movie',
         plex_request: false,
         description: data.overview ? data.overview : 'No description available',
         genre_ids: data.genres.map((genre) => genre.id),
-
-        release_date: data.release_date ? data.release_date : data.first_air_date,
+        release_date: isMovieDetail(data) ? data.release_date : data.first_air_date,
     };
 
     return (
         <div className="container mx-auto p-6 md:p-8 lg:p-12">
             <BackButton className="mb-4" />
 
-            <div className="grid grid-cols-1 gap-6 md:grid-cols-2">
+            <div className="flex w-full gap-6 md:grid-cols-2">
+                {/* Hero box */}
+                <div className="w-full flex-col sm:flex-row">
+
                 {/* Poster Image */}
-                <div className="relative rounded-lg overflow-hidden shadow-lg">
+                <div className="relative rounded-lg overflow-hidden w-full sm:w-1/3 shadow-lg border border-primary">
                     <Image
-                        src={`https://image.tmdb.org/t/p/w500${data.poster_path}`}
-                        alt={data.title ?? data.name}
+                        src={`https://image.tmdb.org/t/p/w600_and_h900_bestv2${data.poster_path}`}
+                        alt={isMovieDetail(data) ? data.title : data.name}
                         className="w-full h-auto"
-                        width={500}
-                        height={750}
+                        width={600}
+                        height={900}
                         priority
                     />
-                    {data.release_date &&
-                    <div className="absolute bottom-0 left-0 bg-black bg-opacity-70 text-white text-sm p-2">
-                        Release Date: {data.release_date}
-                    </div>
-                    }
-                    {data.last_air_date &&
-                    <div className="absolute bottom-0 left-0 bg-black bg-opacity-70 text-white text-sm p-2">
-                        Last Air Date: {data.last_air_date}
-                    </div>
-                    }
+                    {isMovieDetail(data) && data.release_date && (
+                        <div className="absolute bottom-0 left-0 bg-black bg-opacity-70 text-white text-sm p-2">
+                            Release Date: {data.release_date}
+                        </div>
+                    )}
+                    {isTvDetail(data) && data.last_air_date && (
+                        <div className="absolute bottom-0 left-0 bg-black bg-opacity-70 text-white text-sm p-2">
+                            Last Air Date: {data.last_air_date}
+                        </div>
+                    )}
                 </div>
 
                 {/* Movie Details */}
                 <div className="space-y-6">
-                    <h1 className="text-3xl font-bold">{data.title ?? data.name}</h1>
+                    <h1 className="text-3xl font-bold">{isMovieDetail(data) ? data.title : data.name}</h1>
                     <p className="text-gray-600">{data.tagline}</p>
                     <AddWatchlistButton media={addButtonData} width="w-1/3" />
-                    <p className="text-gray-700">{data.overview}</p>
+                    <p className="text-gray-700 w-96">{data.overview}</p>
 
-                    <div className="flex items-center gap-2">
+                    <div className="flex max-w-md items-center gap-2">
                         <Label htmlFor="streaming">Streaming on</Label>
-                        <ProvidersBlock tmdbId={data.id} tmdbType={tmdbType} iconSize={48} />
+                        <ProvidersBlock tmdbId={data.id} tmdbType={tmdbType} iconSize={48}  />
                     </div>
 
                     {data.vote_average > 0 && (
@@ -108,28 +114,32 @@ const DetailPage = async ({ params }: DetailPageProps) => {
                     ) : (
                         <p>No genres available</p>
                     )}
+                </div>
+                    </div>
+                </div>
+                <div>
+                <div>
 
                     <p>Language: {data.original_language}</p>
-                    {tmdbType === 'movie' &&
+                    {isMovieDetail(data) && (
                         <>
                             <p>Release Date: {data.release_date}</p>
                             <p>Revenue: ${data.revenue.toLocaleString()}</p>
                             <p>Budget: ${data.budget.toLocaleString()}</p>
                         </>
-                    }
-                    {tmdbType === 'tv' &&
+                    )}
+                    {isTvDetail(data) && (
                         <>
                             <p>First Air Date: {new Date(data.first_air_date).toLocaleDateString()}</p>
                             <p>Last Air Date: {new Date(data.last_air_date).toLocaleDateString()}</p>
-                            <p>Next Air Date: {data.next_episode_to_air_date ? new Date(data.next_episode_to_air).toLocalDateString() : <span className='font-bol'>Series Complete</span>}</p>
+                            <p>Next Air Date: {data.next_episode_to_air ? new Date(data.next_episode_to_air.air_date).toLocaleDateString() : <span className='font-bold'>Series Complete</span>}</p>
                             <p>Number of episodes: {data.number_of_episodes}</p>
                             <p>Number of seasons: {data.number_of_seasons}</p>
                         </>
-                    }
-
-
+                    )}
                     <p>Status: {data.status}</p>
-                    <p>Runtime: {data.runtime ?? data.episode_run_time} minutes</p>
+                    
+                    <p>Runtime: {isMovieDetail(data) ? data.runtime : data.episode_run_time[0]} minutes</p>
                 </div>
             </div>
 
@@ -170,9 +180,7 @@ const DetailPage = async ({ params }: DetailPageProps) => {
             <CastAndCrew cast={data.credits.cast} type="cast" />
 
             {/* External Links */}
-            {/* External Links */}
             <div className="flex justify-center w-full">
-
                 <div className="pt-6">
                     <h3 className="text-lg font-bold text-center">External Links</h3>
                     <div className="flex divide-x divide-gray-400 justify-center">
@@ -198,8 +206,6 @@ const DetailPage = async ({ params }: DetailPageProps) => {
                     </div>
                 </div>
             </div>
-
-
         </div>
     );
 };
